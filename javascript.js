7a87ec36470338fd1f9044e1412010bcf9a63473
Vijayan.N <146306130+alanwalker129@users.noreply.github.com>
2023-11-11 06:47:13 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,8 +1,8 @@
 // add class navbarDark on navbar scroll
 const header = document.querySelector('.navbar');
 console.log(header)
-window.onscroll = function() {
-    const top = window.scrollY;
+mobile.onscroll = function() {
+    const top = mobile.scrollY;
     if(top >=100) {
         header.classList.add('navbarDark');
     }
